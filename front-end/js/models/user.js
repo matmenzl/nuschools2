7805c1d06f5602b06c672bab22229daefe665527
matmenzl <info@mathiasmenzl.ch>
2016-05-29 14:06:36 +0100

    express with jwt

---
+++ front-end/js/models/user.js
@@ -0,0 +1,17 @@
+angular
+  .module('logging')
+  .factory('User', User);
+
+User.$inject = ['$resource', 'API'];
+function User($resource, API){
+
+  return $resource(
+    API+'/users/:id', {id: '@id'},
+    { 'get':       { method: 'GET' },
+      'save':      { method: 'POST' },
+      'query':     { method: 'GET', isArray: false},
+      'remove':    { method: 'DELETE' },
+      'delete':    { method: 'DELETE' }
+    }
+  );
+}
